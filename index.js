c91299c50e0c725cd7052c83196391778d8a0073
noob-ubaid <ubaidurrahman661@gmail.com>
2025-01-31 14:49:40 +0200

    Arrival section done

---
+++ index.js
@@ -0,0 +1,21 @@
+gsap.from("#banner",{
+    y:100,
+    duration:0.9,
+    stagger:0.3,
+    delay:0.4,
+    opacity:0,
+})
+gsap.from("#menu",{
+    y:200,
+    duration:0.9,
+    stagger:0.3,
+    delay:0.4,
+    opacity:0,
+})
+gsap.from("#img",{
+    y:100,
+    duration:1,
+    stagger:0.3,
+    delay:0.6,
+    opacity:0,
+})
